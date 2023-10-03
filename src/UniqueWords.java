139544a179e70480e63bbdb920e6c18d1cf39d3a
ZeyuPeng22 <zeyu.peng22@my.stjohns.edu>
2023-10-02 20:38:37 -0400

    Completed countUnique methon

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -12,10 +12,22 @@ public class UniqueWords
 	  int count = 0;
 	  
       for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
+      {		
+    	  boolean isUnique = true;
+    	  
+    	  for (int j = i + 1; j < list.size(); j++)
 		 {
-			
+			if (list.get(i).equals(list.get(j)))
+			{
+				isUnique = false;
+				break;
+			}
 		 }
+      
+      if (isUnique)
+      {
+    	  count++;
+      }
       }
 	  return count;
    }
